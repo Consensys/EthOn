c10ff077eec91265ec30a42ad75951e7cba34f24
Johannes <jo-tud@users.noreply.github.com>
2017-09-01 10:06:56 +0200

    Update build_spec.sh

--- build_spec.sh
+++ build_spec.sh
@@ -4,4 +4,4 @@
 BUILD_NUM=$(curl -s 'https://api.travis-ci.org/repos/ConsenSys/EthOn/builds' | grep -o '^\[{"id":[0-9]*,' | grep -o '[0-9]' | tr -d '\n')
 echo $BUILD_NUM
 # Restart last project build
-echo curl -X POST https://api.travis-ci.org/builds/$BUILD_NUM/restart --header "Authorization: token "$AUTH_TOKEN
+curl -X POST https://api.travis-ci.org/builds/$BUILD_NUM/restart --header "Authorization: token "$AUTH_TOKEN
