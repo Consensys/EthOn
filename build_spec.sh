b1694db8ec7cb94e7a55927f091dff226a92b7a7
Johannes <jo-tud@users.noreply.github.com>
2017-09-01 09:26:38 +0200

    Child build script
    see https://github.com/ajdm/travis-build-children

---
+++ build_spec.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Get last child project build number
+BUILD_NUM=$(curl -s 'https://api.travis-ci.org/repos/ConsenSys/EthOn/builds' | grep -o '^\[{"id":[0-9]*,' | grep -o '[0-9]' | tr -d '\n')
+# Restart last project build
+curl -X POST https://api.travis-ci.org/builds/$BUILD_NUM/restart --header "Authorization: token "$AUTH_TOKEN
