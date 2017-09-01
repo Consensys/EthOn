495a51d0722a98b87f2fc36c5750c9b184103d97
Johannes <jo-tud@users.noreply.github.com>
2017-09-01 10:06:22 +0200

    Update build_spec.sh

--- build_spec.sh
+++ build_spec.sh
@@ -2,6 +2,6 @@
 
 # Get last child project build number
 BUILD_NUM=$(curl -s 'https://api.travis-ci.org/repos/ConsenSys/EthOn/builds' | grep -o '^\[{"id":[0-9]*,' | grep -o '[0-9]' | tr -d '\n')
-echo $BUILD-NUM
+echo $BUILD_NUM
 # Restart last project build
 echo curl -X POST https://api.travis-ci.org/builds/$BUILD_NUM/restart --header "Authorization: token "$AUTH_TOKEN
